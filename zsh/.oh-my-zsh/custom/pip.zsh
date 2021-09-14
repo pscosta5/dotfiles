3fb97553740dd8ec8e5c2f260c1390980c074c74
Paulo S. Costa <pauloscosta5@gmail.com>
2021-09-14 11:41:43 -0700

    [Zsh] Add PIP_REQUIRE_VIRTUALENV

--- zsh/.oh-my-zsh/custom/pip.zsh
+++ zsh/.oh-my-zsh/custom/pip.zsh
@@ -5,3 +5,5 @@ alias pip_uninstall_all="python -m pip freeze --local | xargs pip uninstall --ye
 alias pip_upgrade_all="python -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 python -m pip install --upgrade"
 
 eval $(pip completion --zsh)
+
+export PIP_REQUIRE_VIRTUALENV=true
